6d4b43c5a41d7d0e2f58c39fa6d8354bc2256fac
minjun0622 <mseo00@stuy.edu>
2019-04-08 14:22:12 -0400

    Skeleton code.

---
+++ ExpressionTree.java
@@ -0,0 +1,44 @@
+
+public class ExpressionTree{
+
+
+
+  /*return the expression as an infix notation string with parenthesis*/
+  /* The sample tree would be: "(3 + (2 * 10))"     */
+  public String toString(){
+    /*you are to write this method*/
+    return "";
+  }
+
+  /*return the expression as a postfix notation string without parenthesis*/
+  /* The sample tree would be: "3 2 10 * +"     */
+  public String toStringPostfix(){
+    /*you are to write this method*/
+    return "";
+  }
+
+  /*return the expression as a prefix notation string without parenthesis*/
+  /* The sample tree would be: "+ 3 * 2 10"     */
+
+  public String toStringPrefix(){
+    /*you are to write this method*/
+    return "";
+  }
+
+
+
+  /*return the value of the specified expression tree*/
+
+  public double evaluate(){
+    /*you are to write this method*/
+    return 0.0;
+
+    }
+
+
+  /*use the correct operator on both a and b, and return that value*/
+  private double apply(char op, double a, double b){
+    /*you are to write this method*/
+    return 0.0;
+
+    }
